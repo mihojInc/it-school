b5f84719410391910f2086f8b6be57cb1c6090c7
alona <alona.hryhorieva@gmail.com>
2018-06-05 22:29:37 +0300

    added comments

--- src/com/lessons/Month.java
+++ src/com/lessons/Month.java
@@ -1,6 +1,9 @@
 package com.lessons;
 
 public enum Month {
+    /**
+     * list of months
+     */
     JANUARY ("январь", 31),
     FEBRUARY ("февраль", 28),
     MARCH ("март", 31),
@@ -14,18 +17,35 @@ public enum Month {
     NOVEMBER ("ноябрь", 30),
     DECEMBER ("декабрь", 31);
 
+    /**
+     * var ruName set name of month in russian;
+     * var days set quantity of days in each month;
+     */
     private String ruName;
     private int days;
 
+    /**
+     * constructor sets russian name and quantity of days for each month
+     * @param ruName
+     * @param days
+     */
     Month(String ruName, int days){
         this.ruName = ruName;
         this.days = days;
     }
 
+    /**
+     * method gets russian name
+     * @return
+     */
     public String getRuName() {
         return ruName;
     }
 
+    /**
+     * method gets quantity of days
+     * @return
+     */
     public int getDays() {
         return days;
     }
--- src/com/lessons/Season.java
+++ src/com/lessons/Season.java
@@ -2,24 +2,43 @@ package com.lessons;
 
 
 public enum Season {
+    /**
+     * list of seasons
+     */
     WINTER ("зима", new Month[]{Month.DECEMBER, Month.JANUARY, Month.FEBRUARY}),
     SPRING ("весна", new Month[]{Month.MARCH, Month.APRIL, Month.MAY}),
     SUMMER ("лето", new Month[]{Month.JUNE, Month.JULY, Month.AUGUST}),
     AUTUMN ("осень",new Month[]{Month.SEPTEMBER, Month.OCTOBER, Month.NOVEMBER});
 
-
+    /**
+     * var ruName set name of season in russian;
+     * var month set list of months for each season
+     */
     private String ruName;
     private Month[] month = new Month[3];
 
+    /**
+     * constructor sets russian name and list of months for each season
+     * @param ruName
+     * @param month
+     */
     Season(String ruName, Month[] month){
         this.ruName = ruName;
         this.month = month;
     }
 
+    /**
+     * method gets russian name
+     * @return
+     */
     public String getRuName(){
         return ruName;
     }
 
+    /**
+     * method gets list of months
+     * @return
+     */
     public Month[] getMonth(){
         return month;
     }
