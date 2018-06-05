bb74036a9363c9a96d3a394c3a07076ee7d03757
YOGA <andrey_1989AD@mail.ru>
2018-06-05 07:01:10 +0300

    добавлен покупатель

--- src/lesson4/phop/Buyer.java
+++ src/lesson4/phop/Buyer.java
@@ -2,5 +2,13 @@ package lesson4.phop;
 
 public class Buyer {
 
+    private Item[] list ;
+    private int money ;
+    private Integer count1;
 
+    Buyer(Integer goodsList, int money){
+        list = new Item[goodsList];
+        this.money = money;
+        count1 = 0;
+    }
 }
