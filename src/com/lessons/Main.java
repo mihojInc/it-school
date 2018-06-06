eee6ac23d40b6d87bf2dfd3bd9aa79c255554319
Alex <alex.kuzminov90@gmail.com>
2018-06-06 11:14:25 +0300

    Revert "First"
    This reverts commit 8ebf812

--- src/com/lessons/Main.java
+++ src/com/lessons/Main.java
@@ -1,55 +1,8 @@
-package com.company;
-import java.util.Scanner;
+package com.lessons;
 
 public class Main {
 
-    public static void main(String[] args) {
-        // write your code here
-        //Crearte new scanner for save answer of choise and shop settings
-        Scanner ans1 = new Scanner(System.in);
-        Scanner cons = new Scanner(System.in);
-        String ans;
-
-        //ask question and set settings of the shop
-        System.out.println("What count of doods do we have: ");
-        int countGoods = cons.nextInt();
-        System.out.println("How much money do we have in cash: ");
-        double mon = cons.nextDouble();
-
-        //create new shop and send main settings in constructor
-        Shop shop = new Shop(countGoods, mon);
-
-        //this loop will execute while we don`t press q
-        do{
-            //asq about our action
-            System.out.println("What will we do? \nAdd goods (a) \nBye good (b) \nGet list (g) \nQuit (q)");
-
-            //read answer
-            ans = ans1.nextLine();
-
-            switch (ans){
-                case "a":
-                    //if we press a we add googs
-                    shop.addGoods();
-                    break;
-                case "b":
-                    //if we press b we bye googs
-                    shop.byeGoods();
-                    break;
-                case "g":
-                    //if we press a we print list of goods
-                    shop.listGoods();
-                    break;
-                case "q":
-                    //if we press a we quit
-                    System.out.println("Good bye");
-                    break;
-                default :
-                    //if we press something else we print message about incorrect enter
-                    System.out.println("Your choise is incorrect");
-            }
-
-        }while (ans=="q");
+    public static void main(String[] args){
 
     }
 }
