f01ad85c9ea509715183b1a01e02b43bde43c6bf
mikhailKolomiets <mikhail.kolomiets@gmail.com>
2018-06-17 13:05:54 +0300

    first version FM

---
+++ src/com/lessons/ConsoleCommand.java
@@ -0,0 +1,50 @@
+package com.lessons;
+
+/**
+ * Represent the console command and descriptor by introduced value
+ */
+public enum ConsoleCommand {
+    N0_COMMAND(null, "no command"),
+    H("h", "get help"),
+    A("a", "add new player"),
+    V("v", "view all players"),
+    VS("V", "view all players by skill"),
+    P("p", "play football match"),
+    D("d", "delete player"),
+    Q("q", "quit");
+
+    /**
+     * Console representation
+     */
+    private String value;
+
+    /**
+     * Some information about this command
+     */
+    private String description;
+
+    ConsoleCommand(String value, String description) {
+        this.value = value;
+        this.description = description;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Convert string value to his ConsoleCommand issue
+     *
+     * @return enum by parsing sting value of NO_COMMAND if it d't exist
+     */
+    public ConsoleCommand parse(String value) {
+        for (ConsoleCommand c : values())
+            if (c.getValue() != null && c.getValue().equals(value))
+                return c;
+        return N0_COMMAND;
+    }
+}
---
+++ src/com/lessons/Player.java
@@ -0,0 +1,77 @@
+package com.lessons;
+
+/**
+ * Football player
+ */
+public class Player {
+
+    private String name;
+    private byte age = 18;
+    private String team = "Street";
+    private int rang = 10;
+
+    public Player() {
+    }
+
+    public Player(String name, byte age, String team, int rang) {
+        this.name = name;
+        this.age = age;
+        this.team = team;
+        this.rang = rang;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public byte getAge() {
+        return age;
+    }
+
+    public void setAge(byte age) {
+        this.age = age;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public int getRang() {
+        return rang;
+    }
+
+    public void setRang(int rang) {
+        this.rang = rang;
+    }
+
+    /**
+     * @return true only if all fields equals
+     */
+    @Override
+    public boolean equals(Object o) {
+
+        if (o instanceof Player) {
+            Player player = (Player) o;
+            if (player.getName().equals(this.getName()) && player.getAge() == this.getAge()
+                    && player.getRang() == this.getRang()) {
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode() + age + rang;
+    }
+}
