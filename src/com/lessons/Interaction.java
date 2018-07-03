f29244296c2551c72b1dcf5ff76c43ad8044f069
AlinaAlbasova <albasovaalina@gmail.com>
2018-07-03 15:40:30 +0300

    i correct it

--- src/com/lessons/Interaction.java
+++ src/com/lessons/Interaction.java
@@ -24,29 +24,15 @@ public class Interaction {
                 System.out.println("Noooooothinggggg");
             }
             if (result.equals("addTeam")) {
-                System.out.println("Add information about team in such form:");
-                System.out.println("[nameOfTeam] [instructor] [country]");
-                if (inp.hasNext()) {
-                    System.out.println("Inp has next"+inp.nextLine());
-                    newFootballManager.addNewTeam(inp);
-                }
+                addTeam(inp);
             } else if (result.equals("addPlayer")) {
-                System.out.println("Add information about player in such form:");
-                System.out.println("[nameOfPlayer] [age] [rating] [team]");
-                if (inp.hasNext()) {
-                    inp.nextLine();
-                    String userInput = inp.nextLine();
-                    newFootballManager.addNewPlayer(userInput );
-                }
-
+                addPlayer(inp);
             } else if (result.equals("unique")) {
                 newFootballManager.uniquePlayes();
 
 
             } else if (result.equals("byRating")) {
                 newFootballManager.sortedByRating();
-
-
             } else if (result.equals("deletePlayer")) {
                 newFootballManager.sortedByRating();
                 System.out.println("Enter player to delete");
@@ -61,20 +47,7 @@ public class Interaction {
 
 
             } else if (result.equals("play")) {
-                System.out.println("There are available teams:");
-                newFootballManager.displayTeams();
-                System.out.println("Enter names of teams:");
-                if(inp.hasNext()){
-                    inp.nextLine();
-                    String firstTeam = inp.nextLine();
-                    System.out.println(firstTeam);
-                    String secondTeam = inp.nextLine();
-                    System.out.println(secondTeam);
-                    newFootballManager.game(firstTeam,secondTeam);
-                }
-
-
-
+                playGame(inp);
             } else if (result.equals("exit")) {
                 System.out.println("Good Bye");
                 break;
@@ -94,5 +67,35 @@ public class Interaction {
             }
         }
         return null;
+    }
+    private void addTeam(Scanner inp){
+        System.out.println("Add information about team in such form:");
+        System.out.println("[nameOfTeam] [instructor] [country]");
+        if (inp.hasNext()) {
+            System.out.println("Inp has next"+inp.nextLine());
+            newFootballManager.addNewTeam(inp);
+        }
+    }
+    private void addPlayer(Scanner inp){
+        System.out.println("Add information about player in such form:");
+        System.out.println("[nameOfPlayer] [age] [rating] [team]");
+        if (inp.hasNext()) {
+            inp.nextLine();
+            String userInput = inp.nextLine();
+            newFootballManager.addNewPlayer(userInput );
+        }
+    }
+    private void playGame(Scanner inp){
+        System.out.println("There are available teams:");
+        newFootballManager.displayTeams();
+        System.out.println("Enter names of teams:");
+        if(inp.hasNext()){
+            inp.nextLine();
+            String firstTeam = inp.nextLine();
+            System.out.println(firstTeam);
+            String secondTeam = inp.nextLine();
+            System.out.println(secondTeam);
+            newFootballManager.game(firstTeam,secondTeam);
+        }
     }
 }
