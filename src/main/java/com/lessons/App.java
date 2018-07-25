7199aa0564936693aacd32e1bb7514fe4a905f0b
Klimenko-Yuri <yuri.klimenko@gmail.com>
2018-07-25 16:34:00 +0300

    First without comments

--- src/main/java/com/lessons/App.java
+++ src/main/java/com/lessons/App.java
@@ -2,6 +2,23 @@ package com.lessons;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Shop store = new Shop();
+
+        Provider provider1= new Provider(store, "ATB", TypeItems.FISH, 100);
+        Provider provider2= new Provider(store, "ROST", TypeItems.MEET, 500);
+        Provider provider3= new Provider(store, "KLASS", TypeItems.MILK, 900);
+
+        Сustomer сustomer1 = new Сustomer("Vasya", store, 1200);
+        Сustomer сustomer2 = new Сustomer("Petya", store, 300);
+
+
+        new Thread(provider1).start();
+        new Thread(provider2).start();
+        new Thread(provider3).start();
+        new Thread(сustomer1).start();
+        new Thread(сustomer2).start();
+
+
     }
 }
