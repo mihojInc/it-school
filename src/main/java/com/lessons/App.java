145e207a6c8272d79bdd234f46d6bc052ac9c2bc
tsapkojava <tsapko.java@gmail.com>
2018-07-29 23:30:45 +0300

    full functional 2

--- src/main/java/com/lessons/App.java
+++ src/main/java/com/lessons/App.java
@@ -1,51 +1,37 @@
 package src.main.java.com.lessons;
 
-import src.main.java.com.lessons.EmptyURLException;
-import src.main.java.com.lessons.Loader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.*;
+import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws EmptyURLException, IOException, InterruptedException{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        MenuLoader menuLoader = MenuLoader.A;
         Loader loader = new Loader();
-        String[] arrayLink = new String[] {
-                "https://o-viber.ru/wp-content/uploads/2017/08/smeh-18.png",
-                "https://sfw.so/uploads/posts/2011-03/1300730574_ddd-1.jpg",
-                "http://w-hatsapp.ru/wp-content/uploads/2017/05/screenshot-2017-05-19-001-6.png",
-                "http://w-hatsapp.ru/wp-content/uploads/2017/05/7-4.png",
-                "http://img.gazeta.ru/files3/717/3990717/bmw-pic452-452x452-36449.jpg",
-        };
-        ExecutorService service = Executors.newFixedThreadPool(arrayLink.length);
-        List<CallableImageLoader> listThreads = new ArrayList<CallableImageLoader>();
-        for (int i = 0; i<arrayLink.length; i++ ){
-            listThreads.add( new CallableImageLoader(i+1, arrayLink[i]));
-        }
-        CompletionService<String> completionService = new ExecutorCompletionService(service);
-        for (CallableImageLoader elem: listThreads) {
-            if (elem instanceof Callable) {
-                completionService.submit(elem);
-            }
-        }
-        for (int j = 0; j < arrayLink.length; j++) {
-            try {
-                String result = completionService.take().get().toString();
-                System.out.println("result is " + result);
-            } catch (ExecutionException e) {
-                e.printStackTrace();
-            }
-        }
-        service.shutdown();
-        MenuLoader menuLoader =  MenuLoader.A;
-        menuLoader.printMenu();
 
-  /*      for (String link: arrayLink) {
-            loader.setUrlLine(link);
-            loader.load();
-        } */
+            while (menuLoader != menuLoader.Q) {
+
+                switch (menuLoader) {
+
+                    case F:
+                        System.out.println("Uploading from file ...");
+                        loader.fromFile();
+                        break;
+
+                    case C:
+                        System.out.print("Please input path: ");
+                        loader.fromConsole(scanner.nextLine());
+                        break;
+
+                    case A:
+                    default:
+                        menuLoader.printMenu();
+                        break;
+                }
+
+
+                menuLoader = menuLoader.getMenuCommand(scanner.nextLine());
+
+            }
+        }
 
     }
-
-
-}
--- src/main/java/com/lessons/Loader.java
+++ src/main/java/com/lessons/Loader.java
@@ -1,49 +1,124 @@
 package src.main.java.com.lessons;
 
-import src.main.java.com.lessons.EmptyURLException;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
-import java.nio.channels.Channels;
-import java.nio.channels.ReadableByteChannel;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+
+import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Loader {
-    private String urlLine = "";
 
-    // getter for URL
-    public String getUrlLine() {
-        return urlLine;
+    private static String logFile = "C:\\Users\\master\\IdeaProjects\\it-school_task16_image\\src\\main\\java\\com\\lessons\\sourse\\Log";
+
+    public List<String> getUrl(Path path) {
+        List<Path> paths = new ArrayList<>();
+        try {
+            return Files.readAllLines(path);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    //download images from file
+    public void fromFile() {
+        List<Callable<String>> listTask = new ArrayList<>();
+        List<String> listPaths = getUrl(Paths.get("C:\\Users\\master\\IdeaProjects\\it-school_task16_image\\src\\main\\java\\com\\lessons\\sourse\\Links"));
+
+        listPaths = listPaths.subList(0, listPaths.size());
+
+        for (String path : listPaths) {
+            Callable<String> oper = () -> {
+                String name = load(path);
+                return Thread.currentThread().getName() + " loading is complete " + name;
+            };
+            listTask.add(oper);
+        }
+
+        ExecutorService executorService = Executors.newWorkStealingPool();
+        try {
+            List<Future<String>> stringList = executorService.invokeAll(listTask);
+            stringList.stream()
+                    .map(future -> {
+                        try {
+                            return future.get();
+                        } catch (Exception e) {
+                            throw new IllegalStateException(e);
+                        }
+                    })
+                    .forEach(System.out::println);
+
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+  //logging of all operations
+    public void logging(String text) {
+        try {
+            text += "\n";
+            Files.write(Paths.get(logFile), text.getBytes(), StandardOpenOption.APPEND);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    //creating files
+    public String load(String source) {
+
+        String[] strings = source.split("/");
+        String filename = strings[strings.length - 1];
+
+        Path pathToCopy = Paths.get("images\\" + filename);
+        logging(Thread.currentThread().getName() + " download " + filename);
+
+        InputStream inputStream = null;
+
+        try {
+            URL url = new URL(source);
+            inputStream = url.openStream();
+
+            Files.copy(inputStream, pathToCopy, StandardCopyOption.REPLACE_EXISTING);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return filename;
     }
 
-    //setter for URL
-    public void setUrlLine(String urlLine) throws EmptyURLException {
-        if (urlLine == null || urlLine.length() == 0) {
-            throw new EmptyURLException("Link of image is null");
+    //download images from the console
+    public void fromConsole(String url) {
+        if (url.equals("")) {
+            System.out.println("you entered an incorrect link");
+        } else {
+            String name = load(url);
+            System.out.println(name + " loading is complete");
         }
-        this.urlLine = urlLine;
     }
 
-    public void load() throws IOException {
-        URL url = new URL(urlLine);
-        int begin = urlLine.lastIndexOf("/");
-        if (begin == -1) {
-            System.out.println("index of urlLine = -1");
-            return;
+    static {
+        try {
+            if (!Files.exists(Paths.get("images\\"))) {
+                Files.createDirectory(Paths.get("images\\"));
+            }
+            Files.write(Paths.get(logFile), "".getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        //put name of file
-        String name = urlLine.substring(begin + 1, urlLine.length());
-        Path path2 = Paths.get("images");
-        if (!Files.exists(path2)) {
-            Files.createDirectories(path2);
-        }
-        Path downloadPath = Paths.get("images\\" + name);
-        ReadableByteChannel rbc = Channels.newChannel(url.openStream());//reading by byte
-        FileOutputStream fos = new FileOutputStream(downloadPath.toFile()); //stream of symbols for write to file
-        fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
     }
 
 }
